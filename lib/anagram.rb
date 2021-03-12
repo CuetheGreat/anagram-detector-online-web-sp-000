9b9012796df690eeeeecf85c086dfa2d69064dd0
Clayon Stewart <c.stewart89@gmail.com>
2021-03-12 15:58:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,8 +11,8 @@ class Anagram
     
     same_letters = []
     match_cases.collect do |word|
-      puts word
-      puts @match
+      puts word.split(/(\w)/)
+      puts @match.split(/(\w)/)
       if word.length == @match.length && word.split(/(\w)/) == @match.split(/(\w)/)
         
         same_letters << word
