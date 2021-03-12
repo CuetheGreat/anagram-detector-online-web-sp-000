2a137221aee237b74306acfcae2605466a159035
Clayon Stewart <c.stewart89@gmail.com>
2021-03-12 15:22:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :match
+  
+  def initialize(subject)
+    @match = subject
+  end
+  
+  def match(match_cases)
+     matches =  match_cases.collect do |word|
+        match_cases.length == @match.length
+    end
+    
+    puts matches
+  end
+end    
